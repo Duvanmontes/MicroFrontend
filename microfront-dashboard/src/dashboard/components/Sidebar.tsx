2cce0dacdcc9d8770238b07870abc926164d5f73
Duvan Montes <drmontes@unicesar.edu.co>
2024-05-18 23:22:12 -0500

    This commit refactors the Sidebar component and Dashboard page, adjusts styling, and improves code readability.

--- microfront-dashboard/src/dashboard/components/Sidebar.tsx
+++ microfront-dashboard/src/dashboard/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import styled from "styled-components";
@@ -6,12 +6,17 @@ import styled from "styled-components";
 const SidebarContainer = styled.div`
   user-select: none;
   width: 250px;
-  height: 100vh;
+  height: 100%;
   background-image: linear-gradient(to bottom right, #6b46c1, #4299e1);
   color: white;
   position: fixed;
-  top: 0;
+  top: 59px; /* Ajusta esto para alinearlo con el navbar old:64px */
   left: 0;
+  overflow-y: auto;
+  // media para que se oculte cuando la pantalla sea pequeña
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const SidebarHeader = styled.div`
@@ -61,22 +66,21 @@ const Sidebar: React.FC = () => {
       <SidebarHeader>
         <img
           src="https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-512.png"
-          height="50"
-          width="50"
+          height="35"
+          width="35"
+          alt="React Logo"
         />
         Sidebar
       </SidebarHeader>
       <SidebarContent>
-        {["Item 1", "Item 2", "Item 3", "Item 4"].map((e, i) => {
-          return (
-            <SidebarItem key={i}>
-              <span>{e}</span>
-              <div className="sidebar-item-image">
-                <FontAwesomeIcon icon={faChevronRight} />
-              </div>
-            </SidebarItem>
-          );
-        })}
+        {["Item 1", "Item 2", "Item 3", "Item 4"].map((e, i) => (
+          <SidebarItem key={i}>
+            <span>{e}</span>
+            <div className="sidebar-item-image">
+              <FontAwesomeIcon icon={faChevronRight} />
+            </div>
+          </SidebarItem>
+        ))}
       </SidebarContent>
     </SidebarContainer>
   );
